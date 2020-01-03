952eab92cf3ba97516e34d5fd52d613e54d6bf30
ducan0124567 <49425492+ducan0124567@users.noreply.github.com>
2020-01-03 23:57:06 +0700

    Remove_Bin

--- Detect_Text_In_Game/bin/Debug/export.bat
+++
@@ -1,2 +0,0 @@
-Detect_Text_In_Game.exe -e "E:\Tai Lieu Hoc\C\Detect_Text_In_Game\Detect_Text_In_Game\bin\Debug\ori" "E:\Tai Lieu Hoc\C\Detect_Text_In_Game\Detect_Text_In_Game\bin\Debug\export" "Heavy Rain"
-Pause
